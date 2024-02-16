db6a40a717396c1d671eda6996eb355322764e88
Shubham997799 <136079809+Shubham997799@users.noreply.github.com>
2024-02-16 17:06:40 +0530

    Create Program10.java

---
+++ Program10.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Program10
+{
+    public static void main(String args[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("May I know your Name : \n");
+        String sName = sobj.nextLine();
+
+        System.out.println("Welcome " + sName + " at this  place!");
+    }
+}
